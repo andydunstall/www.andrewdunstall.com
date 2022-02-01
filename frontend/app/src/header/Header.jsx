fec997d6d7e071f76fcd818f373ef070c7850c4f
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-02-01 05:36:45 +0000

    header: add linkedin

--- frontend/app/src/header/Header.jsx
+++ frontend/app/src/header/Header.jsx
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import Email from '../icons/Email';
 import GitHub from '../icons/GitHub';
+import Linkedin from '../icons/Linkedin';
 
 import './Header.css';
 
@@ -16,6 +17,7 @@ function Header() {
       <div className="contact">
         <a href="https://github.com/dunstall" aria-label="GitHub" target="_blank" rel="noopener noreferrer"><GitHub /></a>
         <a href="mailto:andydunstall@hotmail.co.uk" aria-label="Email"><Email /></a>
+        <a href="https://uk.linkedin.com/in/andy-dunstall-531b6122a" aria-label="Linkedin"><Linkedin /></a>
       </div>
     </div>
   );
